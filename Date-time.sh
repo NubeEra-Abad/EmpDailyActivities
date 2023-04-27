63fa541ae2893c613a890f3ca44acf6c03dbc569
vishalkhillare <113585079+vishalkhillare@users.noreply.github.com>
2023-04-27 11:06:28 +0530

    Create Date-time.sh

---
+++ Date-time.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a shell script that prints out the current date and time.
+
+# Get the current date and time
+CURRENT_DATE=$(date "+%Y-%m-%d")
+CURRENT_TIME=$(date "+%H:%M:%S")
+
+# Print out the date and time
+echo "The current date is $CURRENT_DATE and the current time is $CURRENT_TIME."
